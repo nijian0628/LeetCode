7b8a18a1f1d45787fb7c061036d4a48feb87f049
unknown <nijian08@gmail.com>
2021-05-25 02:43:51 -0700

    delete lines

--- EachADay/13-RomantoInteger.py
+++ EachADay/13-RomantoInteger.py
@@ -8,9 +8,5 @@
         
         r=dic[s[len(s)-1]]
 
-        for i in range(1,len(s)):
-            if dic[s[i]]<=dic[s[i-1]]:
-                r=r+dic[s[i-1]]
-            else:
-                r=r-dic[s[i-1]]
+
         return r
